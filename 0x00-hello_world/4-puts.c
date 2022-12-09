ea70b1bf39deb11ff7eb17e300ffa8a153fe33b5
oysteve <oysteve>
2022-12-08 17:38:14 -0800

    4 puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,19 +1,16 @@
 #include <stdio.h>
 
 /**
- *
  * * main - Write '"Programming is like building a multilingual puzzle'
- *
  * *Return: Always 0 (success)
- *
  * */
 
 int main(void)
 
 {
 
-puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 
-return (0);
+	return (0);
 
 }
