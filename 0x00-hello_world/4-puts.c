9e5f9eee3cc503f9f1056e5a2ebb1b3580c0814d
oysteve <oysteve>
2022-12-08 17:20:40 -0800

    puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#!/bin/bash
+#include <studio.h>
+
+/**
+ * main -prints a given phrase
+ * Return:0 if exited properly, non-zero otherwise
+ */
+
+init main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle"0;
+	return (0);
+}
