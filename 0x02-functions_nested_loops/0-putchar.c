366ede55e987d623d2d38584287d02b7a958edd7
oysteve <oysteve>
2022-12-21 22:09:49 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,39 +1,15 @@
-#include "main.h"
-
-
+#include <stdio.h>
 
 /**
+ * main - A program that prints _putchar.
  *
- *  * main - print _putchar
+ * Return: Always 0 (success)
  *
- *   * is written by us
- *
- *    * Return: Always 0 (Success)
- *
- *     */
+ */
 
-int main(void)
-
+int main()
 {
-
-		_putchar('_');
-
-			_putchar('p');
-
-				_putchar('u');
-
-					_putchar('t');
-
-						_putchar('c');
-
-							_putchar('h');
-
-								_putchar('a');
-
-									_putchar('r');
-
-										_putchar('\n');
-
-											return (0);
-
+	char a[10] = "_putchar"
+	printf("%s", a);
+	return;
 }
