a9d8f83377f6191572f4f00309f616720934b1a8
oysteve <oysteve>
2022-12-23 22:13:34 -0800

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main-printing alphaber 10times
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+	char alphabet[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i, j;
+	i = 0;
+	while (i < 10)
+	{
+		j = 0;
+		while (j < 26)
+		{
+			_putchar(alphabet[j]);
+			j++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}		
