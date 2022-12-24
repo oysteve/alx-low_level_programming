c63f66ee916a576b24672b191d4ebf49696fd85f
oysteve <oysteve>
2022-12-23 21:13:16 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,17 +6,17 @@
  * Description: Doing functions in c.
  *
  * Return: Always 0 (Success)
- */         
+ */
 
 int main(void)
 {
 	int c = 0;
-	char ade[10] = "_putchar\n";	
-	
+	char ade[10] = "_putchar\n";
+
 	while (c < 9)
 	{
 		_putchar(ade[c]);
 		c++;
 	}
 	return (0);
-}	
+}
