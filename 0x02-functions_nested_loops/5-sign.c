7c401adbff4e7fb9734a192e07ff1f3284ecf718
oysteve <oysteve>
2022-12-23 22:42:29 -0800

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - if else application.
+ *
+ */
+
+int print_sign(int n);
+{
+	if (n > 0)
+	{
+		return 1;
+	}
+	else if (n = 0)
+	{
+		return 0;
+	}
+	else
+	{
+		return -;
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -10,5 +10,6 @@
 int _putchar(char);
 void print_alphabet(void);
 void print_alphabet_x10(void);
+int print_sign(int n);
 
 #endif
