01585496c7dbc939b0ee912622f77c17fb9246f2
oysteve <oysteve>
2022-12-24 20:53:45 -0800

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main-addition function
+ * @a: number to be added
+ * @b: number to be added
+ * @sum: addition of the two numbers
+ * Return: sum (success)
+ *
+ */
+
+int add(int a, int b)
+{
+	int sum = a + b;
+
+		return (sum);
+}
