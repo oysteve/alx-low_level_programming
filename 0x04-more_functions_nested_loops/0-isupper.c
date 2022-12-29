fc8d3ca322e1da7e9b6f36fbe263462ab911ea23
oysteve <oysteve>
2022-12-29 02:40:26 -0800

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include <stdlib,h>
+#include "main.h"
+
+int _isupper(int c)
+{
+	if (c = 'A' && c = 'Z')
+	{
+		return(1);
+	}
+	else
+	{
+		return(0);
+	}
+	
+int main()
+{
+	_putchar_i(_isupper);
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,6 @@
+#ifndef main_h
+#define main.h
+
+int _isupper(int c);
+
+#endif
