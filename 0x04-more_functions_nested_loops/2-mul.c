0756c59b3f172629872b0bdb9393d77a2a08c25c
oysteve <oysteve>
2022-12-29 03:29:24 -0800

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - multiplication function
+ * @a: first number to be multiplied
+ * @b: second number to be multiplied
+ * Return: returns result of the multiplication
+ */
+
+int mul(int a, int b)
+{
+	result = a * b;
+	return (result);
+}
