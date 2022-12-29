090eae8dcead69111499743e7f6c43595ad00386
oysteve <oysteve>
2022-12-29 03:15:50 -0800

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isdigit - checking for digit
+ * @c: print int
+ * Return: 1 or 0
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
